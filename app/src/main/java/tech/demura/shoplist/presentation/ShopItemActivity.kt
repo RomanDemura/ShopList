3fac982cc91bcd37872a5a9da93ee73490ffc884
Roman Demura <anokhin.roman.27@gmail.com>
2022-04-23 23:54:13 +0300

    Creating ShopItemActivity

---
+++ app/src/main/java/tech/demura/shoplist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package tech.demura.shoplist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import tech.demura.shoplist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
